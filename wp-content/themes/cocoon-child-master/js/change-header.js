271c4a98ddf9d9dc851c6f2d30f26ef04b2732e2
y-nagai0725 <y.nagai0725@gmail.com>
2024-11-20 18:49:15 +0900

    topページ、画像とテキスト更新

--- wp-content/themes/cocoon-child-master/js/change-header.js
+++ wp-content/themes/cocoon-child-master/js/change-header.js
@@ -6,10 +6,8 @@ window.addEventListener("scroll", function () {
   const contentPosition = content.getBoundingClientRect().top + window.scrollY;
   const adjustmentValue = 90;
   if (window.scrollY < contentPosition - adjustmentValue) {
-    navi.classList.add("white");
     mobileHeaderMenu.classList.add("white");
   } else {
-    navi.classList.remove("white");
     mobileHeaderMenu.classList.remove("white");
   }
 });
